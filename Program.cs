1e0415ae420bd213bd3d8795273b7bc6d5c575d9
AleksandrIstrelov <istrelov004@gmail.com>
2023-02-26 16:17:30 +0300

    enter and print aray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Из имеющегося массива строк сформировать массив из строк, длина которых меньше, либо равна 3 символа
+//Первоначальынйй массив вводится с клавиатуры. При решении использовать только массивы
+
+// --------------Начало программы ----------------------
+
+string[] UserArray = GetUserArray("Введите нужные слова. Для завершения ввода нажмите Enter не вводя текст (строка должна быть пустой): ");
+Console.WriteLine($"{String.Join(", ", UserArray)}");
+
+//-----------------Описание методов --------------------
+
+string[] GetUserArray(string Mess)
+{
+ 
+    Console.WriteLine(Mess);
+    string[] resarray = new string[5];  
+    int i = 1;
+
+    while(true)
+    {
+        Console.Write($"{i} е слово:");
+        string userres = Console.ReadLine();
+
+        if(userres == String.Empty) return resarray;
+
+        if(resarray.Length == i)
+        {
+            Array.Resize(ref resarray, resarray.Length + 1);
+        }
+
+        resarray[i-1] = userres;
+        i++;
+    }
+
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i, j, k]}({i},{j},{k}) "); 
+            
+    }
+
+}
